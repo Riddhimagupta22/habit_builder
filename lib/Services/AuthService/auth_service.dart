c0d322b412e89b85ef1773d4c16e0167c75edfc2
Riddhima Gupta <griddhima226@gmail.com>
2025-07-26 09:12:04 +0530

    main changes

--- lib/Services/AuthService/auth_service.dart
+++ lib/Services/AuthService/auth_service.dart
@@ -2,7 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import '../../Comman/nav_bar.dart';
-import '../../Controller/request/user_screen.dart';
+import '../../Comman/test/request/user_screen.dart';
 import 'docdata.dart';
 
 class AuthService {
